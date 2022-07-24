0ee00993cc76527f6fc89141c7293f5d48e5f538
Mahir Tazwar <mahirtaz@gmail.com>
2022-07-24 11:22:23 +0600

    intial practices

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Body = () => {
+  return (
+    <div className={classes.mainBody} >
+        this is a body
+        </div>
+  )
+}
+
+export default Body
---
+++ src/components/Headers/Headers.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../../assests/images/logo.svg'
+import react from '../../assests/react.svg'
+import classes from './Headers.module.css'
+
+const Headers = () => {
+  return (
+    
+       <header>
+        <div className="container react" id="header">
+          <div>
+            <img className={classes.logo} src={logo} alt="Logo" />
+          </div>
+          <div className="textRight">
+            <img src={react} alt="React" class={classes.reactLogo} /><strong
+              >React</strong
+            >
+          </div>
+        </div>
+      </header>
+
+  )
+}
+
+export default Headers
